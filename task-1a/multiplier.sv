536dadb6b3a24569d645785e018317127fc94256
Anamarija Momic <a.momic@student.tugraz.at>
2022-10-23 00:03:28 +0200

    redo task and more testcases

--- task-1a/multiplier.sv
+++ task-1a/multiplier.sv
@@ -15,14 +15,78 @@ logic [7:0] result_present, result_next;
 logic [7:0] counter_present, counter_next;
 
 
-//state register
+// data path
+logic [1:0] result_mux;
+always_comb begin
+  case(result_mux)
+      8'b00: result_next = result_present;
+      8'b01: result_next = result_present + (b_i << counter_present);
+      8'b10: result_next = 0;
+      default: result_next = result_present;
+  endcase
+end
+
+logic [1:0] counter_mux;
+always_comb begin
+  case(counter_mux)
+      8'b00: counter_next = 0;
+      8'b01: counter_next = counter_present + 1;
+      8'b10: counter_next = counter_present;
+  endcase
+end
+
+//output logic
+assign result_o = result_next;
+assign busy_o = (state_present == CALC);
+assign valid_o = (state_present == DONE);
+
+
+//state machine
+always_comb begin
+  state_next = state_present;
+  result_mux = 8'b00;
+  counter_mux = 8'b10;
+
+  case(state_present)
+      INIT: begin
+        result_mux = 8'b10;
+        counter_mux = 8'b00;
+
+        if(start_i) begin
+          state_next = CALC;
+        end
+      end
+
+      CALC: begin
+        counter_mux = 8'b01;
+
+        if(a_i[counter_present]) begin
+          result_mux = 8'b01;
+        end
+
+        if(counter_present == 3) begin
+          state_next = DONE;
+        end
+      end
+
+      DONE: begin
+        result_mux = 8'b00;
+        counter_mux = 8'b00;
+
+        if(start_i) begin
+          result_mux = 8'b10;
+          state_next = CALC;
+        end
+      end
+  endcase
+end
+
+//registers
 always_ff @(posedge clk_i, posedge rst_i) begin
   if(rst_i == 1) begin
     state_present <= INIT;
     result_present <= 8'b00000000;
-    result_next <= 8'b00000000;
     counter_present <= 8'b00000000;
-    counter_next <= 8'b00000000;
     end
   else begin
     state_present <= state_next;
@@ -31,81 +95,4 @@ always_ff @(posedge clk_i, posedge rst_i) begin
   end  
 end
 
-//next state logic
-always_comb begin
-  state_next = state_present;
-   case(state_present)
-      INIT: begin if(start_i ==1) begin
-        state_next = CALC;
-      end
-        else begin
-         state_next = INIT;
-        end
-      end
-      CALC: begin if(counter_present == 3) begin
-        state_next = DONE;
-      end
-        else begin
-          state_next = CALC;
-        end
-      end
-      DONE: begin if(start_i == 1) begin
-        state_next = CALC;
-      end
-        else begin
-          state_next = DONE;
-        end
-      end
-      default: begin 
-        state_next = state_present;
-      end
-endcase
-end
-
-//output logic 
-always_comb begin
-  case(state_present)
-    INIT: begin 
-      busy_o = 0;
-      valid_o = 0;
-      result_present = 8'b00000000;
-      counter_present = 8'b00000000;
-      result_next = 8'b00000000;
-      counter_next = 8'b00000000;
-    end
-    CALC: begin
-      busy_o = 1;
-      valid_o = 0;
-      counter_next = counter_present + 8'b00000001;
-      if(a_i[counter_present] == 1) begin
-        result_next = result_present + (b_i << counter_present);
-      end
-      else begin
-        result_next = result_present;
-      end
-    end
-    DONE: begin
-      busy_o = 0;
-      valid_o = 1;
-      result_next = result_present;
-      counter_next = 8'b00000000;
-      if(start_i == 1) begin
-        result_next = 8'b00000000;
-      end
-      else begin
-        result_next = result_present;
-      end
-    end
-    default: begin
-             busy_o = 0;
-             valid_o = 0;
-             result_present = 8'b00000000;
-             result_next = 8'b00000000;
-             counter_present = 8'b00000000;
-             counter_next = 8'b00000000;
-    end
-  endcase
-end
-
-assign result_o = result_next;
 endmodule
