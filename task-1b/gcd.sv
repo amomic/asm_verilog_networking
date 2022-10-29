379472f87f4259798878380b2793bf3392254e23
Anamarija Momic <a.momic@student.tugraz.at>
2022-10-29 02:06:28 +0200

    submission 1b

--- task-1b/gcd.sv
+++ task-1b/gcd.sv
@@ -9,12 +9,12 @@ module gcd (
   output logic [31:0] result_o
 );
 
-enum logic [3:0] {INIT,SA, SB, S1, LOOP1, LOOP2, LOOP3,IFinLOOP,BCHECK, DONE} state_next, state_present;
+enum logic [4:0] {INIT,STATE_A0, STATE_B0, S1, LOOP1, LOOP2, LOOP3,IFinLOOP,B_CALC_STATE,BCHECK, DONE} state_next, state_present;
 logic [31:0] a_present, a_next, b_present, b_next, result_present, result_next, k_present, k_next;
-enum logic [2:0] {INPUT_A, SWAP_A, CURRENT_A, A_CALC} a_mux;
-enum logic [2:0] {INPUT_B, SWAP_B, CURRENT_B,BMINUS,BMINU1, B_CALC} b_mux;
-enum logic [2:0] {RESULT_CURRENT, RA, RB, R0,RK} res_mux;
-enum logic [2:0] {K_CURRENT, K0, K_CALC} k_mux;
+enum logic [3:0] {INPUT_A, SWAP_A, CURRENT_A, A_CALC} a_mux;
+enum logic [3:0] {INPUT_B, SWAP_B, CURRENT_B, B_CALC, B_MINUS} b_mux;
+enum logic [3:0] {RESULT_CURRENT, RET_A, RET_B, RES_0,RES_K} res_mux;
+enum logic [3:0] {K_CURRENT, K_0, K_CALC} k_mux;
 
 assign result_o = result_present;
 
@@ -37,48 +37,43 @@ end
 
 
 always_comb begin
-  busy_o = 1'b0;
-  valid_o = 1'b0;
   state_next = state_present;
   a_mux = CURRENT_A;
   b_mux = CURRENT_B;
   res_mux = RESULT_CURRENT;
+  busy_o = 1'b0;
+  valid_o = 1'b0;
   k_mux = K_CURRENT;
 
   case(state_present)
 
       INIT: begin
-        busy_o = 1'b0;
-        valid_o = 1'b0;
-        a_mux = INPUT_A;
-        b_mux = INPUT_B;
-        res_mux = RESULT_CURRENT;
+          a_mux = INPUT_A;
+          b_mux = INPUT_B;
 
           if(start_i) begin
-            state_next = SA;
+            state_next = STATE_A0;
           end
           else begin
             state_next = INIT;
           end
         end
       
-      SA: begin
-        valid_o = 1'b0;
+      STATE_A0: begin
         busy_o = 1'b1;
         if(a_present == 31'b00) begin
-          res_mux = RA;
+          res_mux = RET_A;
           state_next = DONE;
         end
         else begin
-          state_next = SB;
+          state_next = STATE_B0;
         end
       end
 
-      SB: begin
-        valid_o = 1'b0;
+      STATE_B0: begin
         busy_o = 1'b1;
         if(b_present == 31'b00) begin
-          res_mux = RB;
+          res_mux = RET_B;
           state_next = DONE;
         end 
         else begin
@@ -87,12 +82,11 @@ always_comb begin
       end
 
       S1: begin
-        k_mux = K0;
+        k_mux = K_0;
         state_next = LOOP1;
       end
 
       LOOP1: begin
-        valid_o = 1'b0;
         busy_o = 1'b1;
         if(((a_present|b_present)& 2'b01)== 0) begin
           a_mux = A_CALC;
@@ -105,7 +99,6 @@ always_comb begin
       end
 
       LOOP2: begin
-        valid_o = 1'b0;
         busy_o = 1'b1;
         if((a_present & 1'b1) == 0) begin
           a_mux = A_CALC;
@@ -116,7 +109,6 @@ always_comb begin
       end
 
       LOOP3: begin
-        valid_o = 1'b0;
         busy_o = 1'b1;
         if((b_present & 1'b1) == 0) begin
           b_mux = B_CALC;
@@ -127,29 +119,24 @@ always_comb begin
       end
 
       IFinLOOP:begin
-        valid_o = 1'b0;
-        busy_o = 1'b1;
         if(a_present > b_present) begin
           a_mux = SWAP_A;
           b_mux = SWAP_B;
-          b_mux = BMINUS;
-          state_next = BCHECK;
         end
-        else begin
-          b_mux = BMINU1;
-          state_next = BCHECK;
-        end
+          state_next = B_CALC_STATE;
+      end
+
+      B_CALC_STATE: begin 
+        b_mux = B_MINUS;
+         state_next = BCHECK;
       end
 
       BCHECK: begin 
-        valid_o = 1'b0;
-        busy_o = 1'b1;
-        
         if(b_present != 0) begin
           state_next = LOOP3;
         end
         else begin
-          res_mux = RK;
+          res_mux = RES_K;
           state_next = DONE;
         end
       end
@@ -157,11 +144,7 @@ always_comb begin
       DONE: begin
         busy_o = 1'b0;
         valid_o = 1'b1;
-         if(start_i) begin
-          res_mux = R0;
-          state_next = SA;
-        end
-        valid_o = 1'b1;
+        res_mux = RESULT_CURRENT;
         state_next = INIT;
 
       end
@@ -187,12 +170,11 @@ end
 
 always_comb begin
   case(b_mux)
-    INPUT_B: b_next = b_i; 
+    INPUT_B: b_next = b_i;
     SWAP_B: b_next = a_present;
     CURRENT_B: b_next = b_present;
-    BMINUS: b_next = b_next - a_next;
-    BMINU1: b_next = b_present - a_present;
     B_CALC : b_next = b_present >> 2'b01;
+    B_MINUS: b_next = b_present - a_present;
     default: b_next = b_present;
     endcase
 end
@@ -200,22 +182,23 @@ end
 always_comb begin
   case(res_mux)
     RESULT_CURRENT: result_next = result_present;
-    RESULT_CURRENT: result_present = 31'b00;
-    RA: result_next = b_present;
-    RB: result_next = a_present;
-    R0: result_next = 31'b00;
-    RK: result_next = a_present << k_present;
+    RET_A: result_next = b_present;
+    RET_B: result_next = a_present;
+    RES_0: result_next = 31'b00;
+    RES_K: result_next = a_present << k_present;
     default: result_next = result_present;
     endcase
 end
 
+
 always_comb begin
   case(k_mux)
    K_CURRENT: k_next = k_present;
-   K0: k_next = 31'b00;
+   K_0: k_next = 31'b00;
    K_CALC: k_next = k_present + 31'b01;
   default: k_next = k_present;
   endcase
 end
+
 
 endmodule
