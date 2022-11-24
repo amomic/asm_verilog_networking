82c5ab01c94927073ba0d6f04716cdd865aaedc0
Anamarija Momic <a.momic@student.tugraz.at>
2022-11-24 20:25:17 +0100

    continued

--- task-2b/binary_sort_transformed.c
+++ task-2b/binary_sort_transformed.c
@@ -15,53 +15,104 @@ size_t s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;
 // int binary_search(int* A, int low, int high, int x)
 void binary_search()
 {
-   int m = 0; //m
-   a2 = m;
-
-   size_t stacked_s1 = s1; //low
-   t1 = s1;
-
-   size_t stacked_s2 = s2; //high
-   t2 = s2;
-
-   size_t stacked_s3 = s3; //x
-   t3 = s3;
+    size_t saved_s6 = s6;
+    saved_s6 = 0; //m
+    size_t saved_s7 = s7; //for division by shifting
+    saved_s7 = 2;
+    size_t saved_s8 =s8;
+    size_t saved_s9 = s9;
 
 
-   if(t1<=t2) goto input_continue;
-   if(t3<=a0) goto if_continue;
-   a0 = s1;
-   return;
+    if(a2 <= a3) goto inside_first_if;
+    goto first_else;
 
-   input_continue:
-   m = (s1+s2)/2;
-   if(t3 != a0) goto input_if_continue;
-   a0 = a2;
-   return;
+    inside_first_if: saved_s6 = a2+a3;
+    saved_s6 = saved_s6 >> saved_s7;
 
-   if_continue:
-   a0 = s1 +1;
-   return;
-    
-   input_if_continue: 
-    if (t3<a0) goto calculation_one;
-    goto calculation_two; 
-
-    calculation_one: //
+    saved_s8 = a0+saved_s6;
+    if(a4 != saved_s8) goto second_else;
+    a0 = saved_s6;
     return;
 
-    calculation_two: //
+    first_else: 
+    saved_s9 = a0 + a2;
+    if(a4>saved_s9) goto ret1;
+    a0 = a2;
     return;
 
-  size_t t6 = -1;
-  a0 = t6;
-  return;
+    ret1: a0 = a2+1;
+    return;
+
+    second_else: if(a4 < saved_s8) goto ret2;
+    a2 = saved_s6+1;
+    binary_search();
+    a0 = a0;
+    return;
+
+
+    ret2: a3 = saved_s6 -1;
+    binary_search();
+    a0 = a0;
+    return;
+
+return -1;
+
 }
 
 
 // void insertion_sort(int* A, int size)
 void insertion_sort()
 {
+
+    size_t saved_s1 = s1; // i
+    size_t saved_s2 = s2; //x
+    size_t saved_s3 = s3; //idx
+    size_t saved_s4 = s4; //Aidx
+    size_t saved_s5 = s5; //ai
+    t0 = 0; //explicit counter j
+    t1 = 0; // current index
+    t2 = 2; // for shifting
+    t4 = 0; // temp for storing Aj
+    t5 = 0; // temp for Aidx
+    t6 = 0; //temp for storing Ai
+    goto for_loop;
+
+    for_loop: if(t0<a0) goto inside_for;
+    s1 = saved_s1;
+    s2 = saved_s2;
+    s3 = saved_s3;
+    s4 = saved_s4;
+    s5 = saved_s5;
+    return;
+
+    inside_for: t1 = t0;
+    t1 = t1 << t2;
+
+    t4 = a1 + t1;
+    saved_s2 = *(int*)t4;
+
+    a2 = 0;
+    a3 = t1;
+    a4 = saved_s2;
+    binary_search();
+    saved_s3 = a0;
+
+    saved_s1 = t1;
+
+    goto while_loop;
+
+    while_loop: if(saved_s1>saved_s3) goto inside_while;
+    goto lower_for;
+
+    inside_while: saved_s1 = saved_s1 -1;
+    t6 = a1 + saved_s1;
+    saved_s5 = *(int*)t6;
+    if(saved_s1<=saved_s3) goto lower_for;
+    goto while_loop;
+
+    lower_for:  t5 = a1 + saved_s3;
+    t5 = saved_s2;
+    goto for_loop;
 
   return;
 }
@@ -110,9 +161,9 @@ before_output_loop:
     fprintf(stdout, "%08x\n", (unsigned int)*(int*)a1);
 
     // Pointer increment for next iteration
-    a1 = a1 + 4;
+    a1 = a1 + 4; //a1 is A[i]
     // Decrement loop counter
-    a0 = a0 - 1;
+    a0 = a0 - 1; //ao is size
     goto before_output_loop;
 
 after_output_loop:
