5580e7eea65879499fb81c83fba70b0b12db8b46
Anamarija Momic <a.momic@student.tugraz.at>
2023-01-06 00:17:49 +0100

    test cases function

--- task-3a/firewall.cpp
+++ task-3a/firewall.cpp
@@ -15,7 +15,11 @@ struct IPv4Frame
     unsigned short ip_sum;
     uint8_t ip_src[4], ip_dst[4];
     uint8_t options_payload[];
-}__attributte__;
+}__attribute__((__packed__));
+
+int counter_out = 0;
+int counter_in = 0;
+int counter_tcp = 0;
 
 static void hexdump(uint8_t const* data, size_t size)
 {
@@ -69,20 +73,15 @@ FilterResponse processPacket(uint8_t const* packetData, size_t packetSize, Direc
 // TODO: Implement this method!
 
     auto &ip = *reinterpret_cast<const IPv4Frame*>(packetData);
-    int counter_out = 0;
-    int counter_in = 0;
-    bool type = false;
     bool port443 = false;
     bool port80 = false;
-    int counter_tcp = 0;
-    //bool address_dest = false;
-    //bool addres_src = false;
+    bool private_addr_out = false;
+    bool private_addr_in = false;
     
     if((ip.ip_src[0] == (uint8_t)8 && ip.ip_src[1] == (uint8_t)8 && ip.ip_src[2] == (uint8_t)4 && ip.ip_src[3] == (uint8_t)4)
     ||(ip.ip_dst[0] == (uint8_t)8 && ip.ip_dst[1] == (uint8_t)8 && ip.ip_dst[2] == (uint8_t)4 && ip.ip_dst[3] == (uint8_t)4))
     {
         response = DROP;
-        return response;
     }
 
     if(ip.ip_src[0] == (uint8_t)127 && ip.ip_dst[0] == (uint8_t)127)
@@ -90,9 +89,18 @@ FilterResponse processPacket(uint8_t const* packetData, size_t packetSize, Direc
         response = ACCEPT;
     }
     
-    if(ip.ip_off > 0)
+    if((ip.ip_off != 0))
     {
-        response = DROP;
+        if((ip.ip_off == 1)&&!((ip.ip_off>>14) == (unsigned short)1))
+        {
+            response = DROP;
+        }
+
+        if(ip.ip_off > 1)
+        {
+            response = DROP;
+        }
+
     }
     
     if(ip.ip_p !=1  && ip.ip_p != 6 && ip.ip_p != 17)
@@ -100,101 +108,57 @@ FilterResponse processPacket(uint8_t const* packetData, size_t packetSize, Direc
         response = DROP;
     }
 
-    for(int i = 0; i < 4; i++)
+    
+    if((ip.ip_dst[0] == (uint8_t)10) ||( ip.ip_dst[0] == (uint8_t)192 && (ip.ip_dst[1]== (uint8_t)168)) || ((ip.ip_dst[0] == (uint8_t)172 && ((ip.ip_dst[1]>= (uint8_t)16 && ip.ip_dst[1] <= (uint8_t)31) ))))
     {
-        if(!ip.options_payload[i])
-        {
-            type = true;
-        }
-        else
-        {
-            type = false;
-        }
-
-        if(type == false)
-        {
-            break;
-        }
-
-    }
-
-    if(type == true)
-    {
-        for(int i = 4; i < 8; i++)
-        {
-            if(ip.options_payload[4])
-            {
-                type = true;
-                if(!ip.options_payload[i])
-                {
-                    type = true;
-                }
-                else{
-                    type = false;
-                    break;
-                }
-            }
-            else
-            {
-                type = false;
-                break;
-            }
-
-        }
+        private_addr_out = true;
     }
 
 
 
-    if(ip.ip_p == 1)
+     if((ip.ip_src[0] == (uint8_t)10) ||( ip.ip_src[0] == (uint8_t)192 && (ip.ip_src[1]== (uint8_t)168)) || ((ip.ip_src[0] == (uint8_t)172 && ((ip.ip_src[1]>= (uint8_t)16 && ip.ip_src[1] <= (uint8_t)31) ))))
     {
-        if(type && direction == OUTGOING)
+        private_addr_in = true;
+    }
+
+    if((ip.ip_p == 1) && (!((ip.ip_src[0] == (uint8_t)8 && ip.ip_src[1] == (uint8_t)8 && ip.ip_src[2] == (uint8_t)4 && ip.ip_src[3] == (uint8_t)4)
+    ||(ip.ip_dst[0] == (uint8_t)8 && ip.ip_dst[1] == (uint8_t)8 && ip.ip_dst[2] == (uint8_t)4 && ip.ip_dst[3] == (uint8_t)4))))
+    {
+        response = ACCEPT;
+
+        if((ip.options_payload[0] == (uint8_t)8) && direction == OUTGOING && private_addr_out)
         {
             counter_out ++;
-            if (counter_out == 3)
+            if (counter_out % 3 == 0)
             {
-                counter_out = 0;
                 response = DROP;
-                return response;
-            } 
+                
+            }
         }
-        else{
-
-             response = ACCEPT;
- 
-
-        }        
-
-        if(type && direction == INCOMING)
+     
+        if((ip.options_payload[0] == (uint8_t)8) && direction == INCOMING && private_addr_in)
         {
             counter_in ++;
-            if (counter_in == 4)
+            if (counter_in % 4 == 0)
             {
-                counter_in = 0;
                 response = DROP;
+                
                
-            } 
-        }
-        else{
-
-             response = ACCEPT;
-
+            }
         }
     
     }
 
-    if(ip.ip_p == 6)
+    if((ip.ip_p == 6) && (!((ip.ip_src[0] == (uint8_t)8 && ip.ip_src[1] == (uint8_t)8 && ip.ip_src[2] == (uint8_t)4 && ip.ip_src[3] == (uint8_t)4)
+    ||(ip.ip_dst[0] == (uint8_t)8 && ip.ip_dst[1] == (uint8_t)8 && ip.ip_dst[2] == (uint8_t)4 && ip.ip_dst[3] == (uint8_t)4))))
     {
         
-        if(!ip.options_payload[16] && !ip.options_payload[17] && !ip.options_payload[18] && !ip.options_payload[19] && !ip.options_payload[20] && !ip.options_payload[21]&& !ip.options_payload[22]
-        && ip.options_payload[23] && ip.options_payload[24] && !ip.options_payload[25] && ip.options_payload[26] && ip.options_payload[27] && ip.options_payload[28] 
-        && !ip.options_payload[29] && ip.options_payload[31] && ip.options_payload[32])
+        if(((ip.options_payload[2]<<8) | (ip.options_payload[3])) == (uint8_t)433)
         {
             port443 = true;
         }
         
-        if(!ip.options_payload[16] && !ip.options_payload[17] && !ip.options_payload[18] && !ip.options_payload[19] && !ip.options_payload[20] && !ip.options_payload[21]&& !ip.options_payload[22]
-        && !ip.options_payload[23] && !ip.options_payload[24] && ip.options_payload[25] && !ip.options_payload[26] && ip.options_payload[27] && !ip.options_payload[28] 
-        && !ip.options_payload[29] && !ip.options_payload[31] && !ip.options_payload[32])
+        if(((ip.options_payload[2]<<8) | (ip.options_payload[3])) == (uint8_t)80)
         {
             port80 = true;
         }
@@ -202,24 +166,25 @@ FilterResponse processPacket(uint8_t const* packetData, size_t packetSize, Direc
         if(direction==OUTGOING && port443)
         {
             response = DROP;
-           
+            
+            
         }
-        
 
         if(direction == OUTGOING && port80)
         {
-            counter_tcp = counter_tcp+1;
-            if(counter_tcp > 1)
+            counter_tcp++;
+            if(counter_tcp % 2 ==0)
             {
                 response = DROP;
-                counter_tcp = 0;
-            
+                
+            }else
+            {
+                response= ACCEPT;
             }
+        
         }
+
     }
-
-
-
-
+    
     return response;
 }
